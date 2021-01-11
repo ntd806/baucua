7fc22b46d6f755c8b2f7e68a40e16bfcafd2346e
ntd806 <38273875+ntd806@users.noreply.github.com>
2021-01-11 20:34:37 +0700

    Update Database.js

--- server/helper/Database.js
+++ server/helper/Database.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  let sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  let sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 // const sequelize = new Sequelize('database_development', 'root', '', {

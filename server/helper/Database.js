50c99e51c7c5c8ab1fd53f5bac4e033b46b518aa
datnt <datnt@bisync.jp>
2021-01-11 23:37:12 +0700

    [main] fix  config database.

--- server/helper/Database.js
+++ server/helper/Database.js
@@ -2,16 +2,9 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 if (config.use_env_variable) {
-  let sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  let sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
-// const sequelize = new Sequelize('database_development', 'root', '', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   logging: false,
-//   timezone: '+07:00'
-// });
 
 module.exports = sequelize;

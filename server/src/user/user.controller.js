822810867b33f8c86a80755d32eba6c9b83ffa07
dangnghia <nghia.dang@headcapital.com>
2021-01-12 22:55:53 +0700

    [issue_0033] update API

--- server/src/user/user.controller.js
+++ server/src/user/user.controller.js
@@ -91,3 +91,24 @@ async function blockUser(req,res,next) {
     res.status(400).json({ Error: e.message })
   }
 }
+
+async function getAccount(req, res, next) {
+  try {
+    let wallets = await service.getWallet(req.body)
+
+    if(!wallets.length) {
+      return res.status(404).json({
+        success: false,
+        message: 'Entity Not Found'
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      result: wallets,
+      message: ''
+    });
+  } catch (e) {
+    res.status(400).json({ Error: e.message })
+  }
+}

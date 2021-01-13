745ed8586c700122c3e0d87ef3ecfb263d381a09
datnt <datnt@bisync.jp>
2021-01-13 11:36:17 +0700

    [issue_0037] Update UI game pages.

--- server/src/public/js/game.js
+++ server/src/public/js/game.js
@@ -1,11 +1,11 @@
-let startBtn, rabbit, rabbit_position,
+let startBtn, dice, dice_position,
 startTitle, canvas, startTitle_hideTime, 
 height_stop, div_holder, bg_default = 0.0, div_renda, distance = 0.0, 
 count_distance_left, div_bord1, is_finish = false, begin_climb = false,
 panel_finish, div_bord2, div_kiroku, panel_score,
 scale, condition_stop_background = 0.0,
 total_distance_left = 0.0, speed = 0.0, bg_up = 0.0,
-is_sound_climb = false, rabbit_move = 0, is_click = false, rabbit_top, time_set_up, time_flag = 0, time,
+is_sound_climb = false, dice_move = 0, is_click = false, dice_top, time_set_up, time_flag = 0, time,
 soundFinish_Loop = 0, result, flag_sound = false, time_place;
 
 // Keep track of our socket connection
@@ -27,7 +27,7 @@ function setup() {
 
 function draw() {
    // run sound
-   if(!soundsBegin.isPlaying() && startBtn.attribute("state") == 1){
+   if(!soundsBegin.isPlaying()){
       soundsBegin.loop();
    }
 
@@ -89,62 +89,17 @@ function newGame() {
    scale = canvas.width/CANVAS_HEIGHT;
    // define div holder
    div_holder = select("#holder");
-   // define rabbit
-   rabbit = select(".gastan");
-   rabbit.show();
-   rabbit.position(height_stop,height_stop);
-   rabbit.hide();
-   rabbit.attribute('flag', '0');
-   rabbit_top = RABBIT_TOP_FINISH*scale;
+   // define dice
+   dice = select(".gastan");
+   dice.show();
+   dice.position(height_stop,height_stop);
+   dice.hide();
+   dice.attribute('flag', '0');
+   dice_top = dice_TOP_FINISH*scale;
    // set condition stop 
    condition_stop_background = CONDITION_STOP*scale;
    // Set positison background
    bg_default = BACKGROUNG_DEFAULT * scale;
-   // set image bg2
-   bg2 = select(".bg2");
-   var bg2_width = bg2.width*scale;
-   var bg2_height = bg2.height*scale;
-   bg2.style("width",  bg2_width + "px");
-   bg2.style("height", bg2_height + "px");
-   bg2.position(0,bg_default);
-   // set speed of rabbit 
-   speed = DISTANCE*scale/TOTAL_STEPS;
-   // set distance had left
-   total_distance_left = TOTAL_STEPS;
-   // define start title
-   startTitle = select(".start");
-   startTitle.show();
-   startTitle.position(canvas.width + startTitle.width, startTitle.position().y);
-   startTitle_hideTime = millis() + START_TITLE_SHOW_TIME + START_WAITING_TIME;
-   // define start button
-   startBtn = select("#startBtn");
-   startBtn.attribute("state","1");
-   startBtn.attribute('disabled', '');
-   // define count_distance_left
-   count_distance_left = select("#counter")
-   count_distance_left.hide();
-   //count_distance_left.html(total_distance_left);
-   // define div_renda
-   div_renda = select(".renda");
-   div_renda.hide()
-   // define div_bord1
-   div_bord1 = select("#bord1");
-   time_place = TIME_PLACE;
-   time_place = time_place/60;
-   div_bord1.html(time_place.toFixed(2));
-   // define panel_finish
-   panel_finish = select(".finish");
-   panel_finish.hide();
-   // define div_bord2
-   div_bord2 = select("#bord2");
-   div_bord2.hide();
-   // define div_kiroku
-   div_kiroku = select(".kiroku");
-   div_kiroku.hide();
-   // define panel_score
-   panel_score = select(".score");
-   panel_score.hide();
-
 
    var loaddingScreen = select("#loadding");
    loaddingScreen.remove();
@@ -181,7 +136,7 @@ function wellcome(){
  * handle button start to click
  */
 function startBtnClicked(){
-   rabbit.show();
+   dice.show();
    time_flag ++;
    // if(time_flag == 1){
    //    time_set_up =  millis();
@@ -193,13 +148,13 @@ function startBtnClicked(){
       begin_climb = true;
       }
 
-      sound_rabbit_climb();
+      sound_dice_climb();
       show_distance_left();
-      rabbit_changes_image();
-      // Rabbit change position with images change
-      var rabbit_position_recent = rabbit.position();
-      if(rabbit_position_recent.y >= height_stop) {
-         rabbit.position(rabbit_position, rabbit_position_recent.y - speed);
+      dice_changes_image();
+      // dice change position with images change
+      var dice_position_recent = dice.position();
+      if(dice_position_recent.y >= height_stop) {
+         dice.position(dice_position, dice_position_recent.y - speed);
       }
       else {
         bg_default += speed;
@@ -207,101 +162,9 @@ function startBtnClicked(){
       }
       // check stop
       if (bg_default >= condition_stop_background) {
-         set_rabbit_stop();
+         set_dice_stop();
          var requirePoint = new KYTypeRequirement(TYPE_REQUIRE_POINT_1, TYPE_REQUIRE_POINT_2);
          result           = new KYResult(GAME_ID, time, requirePoint);
       }
    }
-}
-
-/**
- * Rabbit changes image
- */
-function rabbit_changes_image(){
-   // Get flag attribute of rabbit
-   var rabbit_flag = rabbit.attribute("flag");
-   if(rabbit_flag == 0){
-      rabbit.attribute('flag', '1');
-      rabbit.attribute("src","img/gastan_2.png");
-   }
-   else {
-      rabbit.attribute('flag', '0');
-      rabbit.attribute("src","img/gastan_1.png");
-   }
-}
-
-/**
- * Effect sound as rabbit stop
- */
-function sound_rabbit_stop(){
-   // Run sound 
-   if (!is_sound_climb) {
-   soundsCancel.stop();
-   }
-}
-
-/**
- * Display finished
- */
-function display_finish(){
-   soundsEnding.play();
-   setInterval(function(){
-      div_bord1.hide();
-      count_distance_left.hide();
-   }, TIME_DELAY_FINISH);
-
-   setTimeout(function(){
-      rabbit.hide();
-      div_bord2.show();
-      panel_finish.show();
-      div_kiroku.show();
-      flag_sound = true;
-   },TIME_DELAY_FINISH);
-}
-
-/**
- * Effect sound as rabbit climbs
- */
-function sound_rabbit_climb(){
-   is_sound_climb = true;
-   // play sound climb
-   if (is_sound_climb) {
-      soundsCancel.play();
-      is_sound_climb = false;
-   }
-}
-
-/**
- * Show distance had left
- */
-function show_distance_left(){
-   // Calculation for distance
-   total_distance_left -= 1;
-   if (total_distance_left == 0) {
-      bg_default = condition_stop_background;
-      is_click = false;
-   }
-   // Distance left
-   count_distance_left.html(total_distance_left);
-   div_renda.show()
-}
-
-/**
- * Set rabbit position stop
- */
-function set_rabbit_stop(){
-   // define is_finish
-   is_finish = true;
-   // Set css for rabbit 
-   rabbit.style('top', rabbit_top +'px');
-   // set image stop and position
-   rabbit.attribute("src","img/gastan_3.png");
-   startBtn.hide();
-   div_renda.hide();
-   sound_rabbit_stop();
-   display_finish();
-}
-
-function keyPressed(){
-   return false;
 }

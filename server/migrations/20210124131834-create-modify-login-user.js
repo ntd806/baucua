a32f067ffab3600ccf3259356de10ac84dbd62c1
ntd806 <ntd806@gmail.com>
2021-01-24 20:27:38 +0700

    Update migration.

---
+++ server/migrations/20210124131834-create-modify-login-user.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('login_users',
+        'logout_at', {
+          type: Sequelize.DATE(3),
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"),
+          allowNull: true,
+          after: "login_at"
+        }
+    );
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('login_users',
+    'logout_at'
+    );
+  }
+};

f70c44f0c75c290f1fef6552f8eb9911ede241d4
datnt <datnt@bisync.jp>
2021-01-25 11:05:03 +0700

    Deploy heroku.

--- public/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import getUrl from 'Constants/url';
-
-export function login(data) {
-  return axios
-    .post(getUrl('login'), data)
-    .then((res) => res.data)
-    .catch(() => {});
-}
--- public/src/services/register.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import getUrl from 'Constants/url';
-
-export function register(data) {
-  return axios
-    .post(getUrl('register'), data)
-    .then((res) => res.data)
-    .catch(() => {});
-}

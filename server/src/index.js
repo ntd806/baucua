579b82542ebc4236ed79416e681bff1b5162f8e4
datnt <datnt@bisync.jp>
2021-01-25 09:04:20 +0700

    [issue_0091]  Update enviroment config.

--- server/src/index.js
+++ server/src/index.js
@@ -61,8 +61,8 @@ app.post('/webhook', async(req, res) => {
 });
 
 
-const ip = "127.0.0.1"; // process.env.IP || "127.0.0.1";
-const port = 3002; // process.env.PORT || 3002;
+const ip = process.env.IP || "127.0.0.1";
+const port = process.env.PORT || 3002;
 
 server.listen(port, ip, function() {
     console.log("Express server listening at %s:%d ", ip, port, process.env.PORT);

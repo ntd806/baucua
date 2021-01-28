5551c9dee1b786aa4ee99558eeed45f00f256836
NMHieu9779 <nmhieu9779@hotmail.com>
2021-01-28 22:18:02 +0700

    integrate api and fix cors

--- public/src/services/register.js
+++ public/src/services/register.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
+import request from './axios';
 import getUrl from 'Constants/url';
 
 export function register(data) {
-  return axios
-    .post(getUrl('register'), data)
+  return request({ isRequestToken: false, method: 'post', url: getUrl('register'), data })
     .then((res) => res.data)
     .catch(() => {});
 }

93715cc2366d723aa57d509aa104610b18e2fef3
datnt <datnt@bisync.jp>
2021-02-03 14:25:49 +0700

    Update run.sh v2.

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Building react......"
-echo cd public
-echo npm run build
-echo pm2 serve build 3000 --spa
+cd public
+npm run build
+pm2 serve build 3000 --spa
 echo "Building nodejs........"
-echo cd ../
-echo pm2 start pm2-apps.json
+cd ../
+pm2 start pm2-apps.json
 echo "Server is starting"

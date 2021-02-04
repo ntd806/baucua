74a464f467245e360d7c725d6f3c9f67782a61c9
ntd806 <ntd806@gmail.com>
2021-02-04 22:11:16 +0700

    [issue_153] Update config.

---
+++ server/src/public/js/config.js
@@ -0,0 +1,5 @@
+const START_WAITING_TIME    = 1000; // milliseconds before starting screen
+const CANVAS_HEIGHT         = 634; // height of canvas
+const TIME_DICE = 100; // Time wait for setup game
+const TIME_SPIN = 8;
+const SPEED = 15;

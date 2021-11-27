6dd224ee3b2d3ee4462ddb57620b0a722aa499f7
Tien Dat <ntd806@gmail.com>
2021-11-27 21:14:13 +0700

    adad

--- server/seeders/20210126164335-user_admins.js
+++ server/seeders/20210126164335-user_admins.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('user_admins', [{
       user_name: 'admin',
-      password: '$2b$10$9MqbXSEa5aXXqkIch./Z4eHKoQX9VIt2P1o0edmSRXMIxhE95w1/e',
+      password: '$2a$10$2/LLCKB9l4nwS8FVCl/I2uddgtRTdGn9a8epOUw1/Gk8AtyAkiZ86',
       token: null,
       login_at: new Date(),
       status: 1,

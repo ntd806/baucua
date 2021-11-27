97f142745a0eb1654e91e881571f34277c3d633b
Tien Dat <ntd806@gmail.com>
2021-11-27 21:02:04 +0700

    đâ

--- server/src/index.js
+++ server/src/index.js
@@ -8,12 +8,6 @@ const path       = require('path');
 const hbs        = require('express-hbs');
 const app        = express();
 const cors = require('cors');
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
-
 app.use(cors())
 
 // Use `.hbs` for extensions and find partials in `views/partials`.

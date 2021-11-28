418003e2d28f4d461a0bd9e570e5025f24ca710f
Tien Dat <ntd806@gmail.com>
2021-11-28 22:39:18 +0700

    dâdaad

--- server/src/index.js
+++ server/src/index.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname + '/public')));
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://cmd386.club');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');

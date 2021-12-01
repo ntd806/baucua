2ce8a6ef022818469cc74c5fd1f1ee2b7c6246a5
Tien Dat <ntd806@gmail.com>
2021-12-01 15:39:50 +0700

    update username

--- public/src/setupProxy.js
+++ public/src/setupProxy.js
@@ -1,11 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const BASE_URL = process.env.BASE_URL;
 
 module.exports = function (app) {
 
   app.use(
     "/api/user/login",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
@@ -13,98 +14,98 @@ module.exports = function (app) {
   app.use(
     "/api/user/register",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/get_account",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/transfers-history",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/get-members",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/post_edit_profile",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/get_all_conversion_rate",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/post_deposit",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/blockUser",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/setting",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/update-setting",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/get-user-history",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/admin/login",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/create-setting",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/user/create-conversion_rates",
     createProxyMiddleware({
-      target: "http://localhost:3002",
+      target: `${BASE_URL}`,
       changeOrigin: true,
     })
   );
--- server/seeders/20210126164335-user_admins.js
+++ server/seeders/20210126164335-user_admins.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('user_admins', [{
       user_name: 'admin',
-      password: '$2b$10$Lj.QuhzuFTTIAnm2ZZUbnuywDEP3d/XcLHl8SADZ8/WZ0tjhEnJei',
+      password: '$2b$10$ZJi38IcUR3A5kgQvKtqMu.FFhl.URb4lVC5rbOGfL/Le/ZkbrL8Ye',
       token: null,
       login_at: new Date(),
       status: 1,

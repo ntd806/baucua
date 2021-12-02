cf29181bbdc30d3065708eb6e10d814f06367e91
Tien Dat <ntd806@gmail.com>
2021-12-02 15:46:51 +0700

    update password

--- server/seeders/20210126164335-user_admins.js
+++ server/seeders/20210126164335-user_admins.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('user_admins', [{
       user_name: 'admin',
-      password: '$2b$10$9MqbXSEa5aXXqkIch./Z4eHKoQX9VIt2P1o0edmSRXMIxhE95w1/e',
+      password: '$2b$10$ZJi38IcUR3A5kgQvKtqMu.FFhl.URb4lVC5rbOGfL/Le/ZkbrL8Ye',
       token: null,
       login_at: new Date(),
       status: 1,
